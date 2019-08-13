0b14a2dff2048805ac0e7bee5d9c98f2f36badb3
Jori Peterson <joripeterson@gmail.com>
2019-08-12 21:27:44 -0600

    Start endpoints for merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,2 +1,10 @@
-class Api::V1::ItemsController < ApplicationController
+class Api::V1::MerchantsController < ApplicationController
+
+	def index
+		render json: Merchant.all
+	end
+
+	def show
+		render json: Merchant.find(params[:id])
+	end
 end
